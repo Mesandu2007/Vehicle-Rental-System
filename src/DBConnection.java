612bcfa1fc16fb4db3e730ddc613ae6b53a9ec43
Mesandu Gunarwardhana <mesandugunarwardhana@gmail.com>
2026-03-17 10:24:38 +0530

    Add files via upload

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/vehicle_rental";
+    private static final String USER = "root";
+    private static final String PASS = "your_password";
+
+    public static Connection getConnection() {
+
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        }
+
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
